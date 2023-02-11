9a3686f7aef614a6a3667fdcbe7a3187388987a4
Fran Linde Blazquez <franlindebl@gmail.com>
2023-02-11 22:04:26 +0100

    Added image slider sample

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
 import TeacherName from './components/TeacherName/TeacherName';
 import Counter from './components/Counter/Counter';
+import ImageSlider from './components/ImageSlider/ImageSlider';
 
 function App() {
+  const imagesOne = [
+    "https://picsum.photos/id/1000/400/200",
+    "https://picsum.photos/id/1002/400/200",
+    "https://picsum.photos/id/1003/400/200",
+    "https://picsum.photos/id/1004/400/200",
+    "https://picsum.photos/id/1005/400/200",
+];
+
+const imagesTwo = [
+  "https://picsum.photos/id/1006/400/200",
+  "https://picsum.photos/id/1008/400/200",
+  "https://picsum.photos/id/1009/400/200",
+  "https://picsum.photos/id/1010/400/200",
+];
+
   return (
     <div className="app">
       <h2>Componente nombre:</h2>
@@ -11,6 +27,10 @@ function App() {
       <h2>Contador:</h2>
       <Counter></Counter>
       <Counter></Counter>
+
+      <h2>Slider de imágenes</h2>
+      <ImageSlider images={imagesOne}></ImageSlider>
+      <ImageSlider images={imagesTwo}></ImageSlider>
     </div>
   );
 }
