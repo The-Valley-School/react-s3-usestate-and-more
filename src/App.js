3fb7f37c64a4b23b7c355379bfe0815e0af7de69
Fran Linde Blazquez <franlindebl@gmail.com>
2023-02-11 20:33:05 +0100

    Add counter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import TeacherName from './components/TeacherName/TeacherName';
+import Counter from './components/Counter/Counter';
 
 function App() {
   return (
     <div className="app">
+      <h2>Componente nombre:</h2>
       <TeacherName></TeacherName>
+
+      <h2>Contador:</h2>
+      <Counter></Counter>
+      <Counter></Counter>
     </div>
   );
 }
